5bf8b76752c6611315b04e9888ae398407483bb8
Lukas Holecek <hluk@email.cz>
2022-09-01 19:19:06 +0200

    Update from Qt Creator (commit 6f3c03f1f9)

--- fakevim/fakevimhandler.h
+++ fakevim/fakevimhandler.h
@@ -1,27 +1,5 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of Qt Creator.
-**
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3 as published by the Free Software
-** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
-** included in the packaging of this file. Please review the following
-** information to ensure the GNU General Public License requirements will
-** be met: https://www.gnu.org/licenses/gpl-3.0.html.
-**
-****************************************************************************/
+// Copyright (C) 2016 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0+ OR GPL-3.0 WITH Qt-GPL-exception-1.0
 
 #pragma once
 
@@ -61,12 +39,6 @@ struct FAKEVIM_EXPORT Range
     int beginPos = -1;
     int endPos = -1;
     RangeMode rangemode = RangeCharMode;
-
-    // Just some convenience functions for compatibility with optional
-    // which is apparently allowed in qtcreator.
-    operator bool() const { return isValid(); }
-    void reset() { beginPos = -1; endPos = -1; }
-    Range &operator*() { return *this; }
 };
 
 struct FAKEVIM_EXPORT ExCommand
