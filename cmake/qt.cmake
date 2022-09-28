84c4cb736c00e8d779f6640cc0f4877bc966ca8c
rewine <luhongxu@uniontech.com>
2022-09-28 17:42:56 +0800

    fix: Qt5::Widgets is invalid for pkgconfig

--- cmake/qt.cmake
+++ cmake/qt.cmake
@@ -3,4 +3,4 @@ find_package(Qt5Widgets REQUIRED)
 include_directories(${Qt5Gui_PRIVATE_INCLUDE_DIRS})
 target_link_libraries(${bin} Qt5::Widgets)
 
-set(${bin}_pkg_config_requires ${${bin}_pkg_config_requires} Qt5::Widgets)
+set(${bin}_pkg_config_requires ${${bin}_pkg_config_requires} Qt5Widgets)
